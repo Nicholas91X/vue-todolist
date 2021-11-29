4dd953a1ed96ce009f1531d1a6d1109d97c02a0e
Nicholas <romitinicholas@gmail.com>
2021-11-29 16:40:10 +0100

    struttura di abse

---
+++ main.js
@@ -0,0 +1,34 @@
+const app = new Vue({
+    el: "#root",
+    data: {
+        content: [
+            {
+                testo: "Fare la lista della spesa",
+                done: false
+            },
+            {
+                testo: "Pagare l'affitto",
+                done: false
+            },
+            {
+                testo: "Andare in palestra",
+                done: false
+            },
+        ],
+        inputValue: ""
+    },
+    methods: {
+        addToDo: function() {
+            if(this.inputValue != "") {
+                this.content.push({
+                    testo: this.inputValue,
+                    done: false
+                });
+                this.inputValue = "";
+            }
+        },
+        removeToDo: function(index) {
+            this.content.splice(index, 1);
+        }
+    }
+})
